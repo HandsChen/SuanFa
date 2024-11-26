44a7d4b19108f6ee81b7e911a92bccd7e28742d9
chen <946586523@qq.com>
2024-11-26 22:58:28 +0800

    fixed: 完成AVL树的基本构建

--- src/java/common/TreeNode.java
+++ src/java/common/TreeNode.java
@@ -4,6 +4,7 @@ public class TreeNode {
     private int val;     // 节点值
     private TreeNode left; // 左子节点引用
     private TreeNode right; // 右子节点引用
+    private int height; //高度
 
     public TreeNode getLeft() {
         return left;
@@ -31,5 +32,13 @@ public class TreeNode {
 
     public void setVal(int val) {
         this.val = val;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
